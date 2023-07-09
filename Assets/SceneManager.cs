fb64756aad6565aaa5883e73f34a7a3617af3e0e
thanhit08Tekville <thanhit08@tekville.com>
2023-07-09 13:04:34 +0900

    Saving video to disk. Tested on Windows version

--- Assets/SceneManager.cs
+++ Assets/SceneManager.cs
@@ -1,9 +1,12 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using LightShaft.Scripts;
 using TMPro;
+using UnityEditor.Media;
 using UnityEngine;
+using UnityEngine.Android;
 using UnityEngine.UI;
 
 public class SceneManager : MonoBehaviour
@@ -19,8 +22,6 @@ public class SceneManager : MonoBehaviour
     //public RawImage cameraPreview;
     public List<string> youtubeURL = new List<string>();
     public List<int> youtubeTime = new List<int>();
-    public string musicClipName; // Name of the music clip to play
-    public string videoSavePath; // File path to save the recorded video
 
 
     private float countdownTime; // Time remaining for the countdown
@@ -31,6 +32,11 @@ public class SceneManager : MonoBehaviour
     private float fadeOutDuration = 1f;
 
     private bool isRecording; // Flag to track if video recording is active
+    public string videoOutputPath = "Assets/Recordings";
+    private string videoFileName = "myVideo.mp4";
+    private string videoOutputRootPath = "F:/2022/Projects/Recordings";
+
+    private MediaEncoder mediaEncoder;
 
     /// <summary>
     /// Song youtube URL index
@@ -50,6 +56,40 @@ public class SceneManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            Debug.Log("Platform: Android");
+        }
+        else if (Application.platform == RuntimePlatform.IPhonePlayer)
+        {
+            Debug.Log("Platform: iOS");
+        }
+        else if (Application.platform == RuntimePlatform.WindowsPlayer || Application.platform == RuntimePlatform.WindowsEditor)
+        {
+            Debug.Log("Platform: Windows");
+        }
+        else if (Application.isEditor)
+        {
+            Debug.Log("Platform: Unity Editor");
+        }
+
+        NativeGallery.CheckPermission(NativeGallery.PermissionType.Write, NativeGallery.MediaType.Video);
+        NativeGallery.CheckPermission(NativeGallery.PermissionType.Write, NativeGallery.MediaType.Audio);
+        NativeGallery.CheckPermission(NativeGallery.PermissionType.Write, NativeGallery.MediaType.Image);
+
+        //if (Application.platform == RuntimePlatform.Android)
+        //{
+        //    // Request necessary permissions
+        //    if (!Permission.HasUserAuthorizedPermission(Permission.Microphone))
+        //    {
+        //        Permission.RequestUserPermission(Permission.Microphone);
+        //    }
+        //    if (!Permission.HasUserAuthorizedPermission(Permission.ExternalStorageWrite))
+        //    {
+        //        Permission.RequestUserPermission(Permission.ExternalStorageWrite);
+        //    }
+        //}
+
         // Add a click event listener to the raw image
         rawImageCollider = rawImage.GetComponent<BoxCollider2D>();
     }
@@ -103,7 +143,7 @@ public class SceneManager : MonoBehaviour
         startButton.SetActive(true);
         isCounting = false;
         isFadingOut = false;
-        isRecording = false;       
+        isRecording = false;
     }
 
     private IEnumerator WaitSystemDoSomething()
@@ -164,11 +204,40 @@ public class SceneManager : MonoBehaviour
                     // Handle the raw image click event here
                     Debug.Log("Raw image clicked!");
                     PauseMusic();
+                    PauseVideoRecording();
+                    return;
                 }
+            }
+            try
+            {
+                Texture2D convertedTexture = ConvertTexture(rawImage.texture);
+
+                mediaEncoder.AddFrame(convertedTexture);
+
+            }
+            catch (Exception)
+            {
+                StopMusic();
             }
         }
 
-        
+
+    }
+
+    private Texture2D ConvertTexture(Texture iTexture)
+    {
+        // Create a new Texture2D with the same dimensions as the RawImage texture
+        Texture2D convertedTexture = new Texture2D(iTexture.width, iTexture.height, TextureFormat.RGBA32, false);
+
+        // Read the pixels from the RawImage texture into the new Texture2D
+        RenderTexture currentRT = RenderTexture.active;
+        RenderTexture renderTexture = new RenderTexture(iTexture.width, iTexture.height, 32);
+        Graphics.Blit(iTexture, renderTexture);
+        RenderTexture.active = renderTexture;
+        convertedTexture.ReadPixels(new Rect(0, 0, iTexture.width, iTexture.height), 0, 0);
+        convertedTexture.Apply();
+        RenderTexture.active = currentRT;
+        return convertedTexture;
     }
 
     public void StartCountdown()
@@ -191,7 +260,8 @@ public class SceneManager : MonoBehaviour
         if (isCounting)
         {
             ResetScene();
-        } else
+        }
+        else
         {
             pauseButton.SetActive(true);
             stopButton.SetActive(true);
@@ -239,10 +309,67 @@ public class SceneManager : MonoBehaviour
         PlayFromUrlFieldStartingAt(youtubeTime[songIndex]);
     }
 
+    public static string GenerateFilename(string songName)
+    {
+        // Remove any special characters from the song name
+        string cleanSongName = RemoveSpecialCharacters(songName);
+
+        // Get the current timestamp
+        string timestamp = DateTime.Now.ToString("yyyyMMdd_HHmmss");
+
+        // Combine the song name and timestamp to create a unique filename
+        string filename = $"{cleanSongName}_{timestamp}.mp4";
+
+        return filename;
+    }
+
+    private static string RemoveSpecialCharacters(string input)
+    {
+        // Remove special characters using regular expressions
+        string pattern = "[^a-zA-Z0-9]";
+        string replacement = "";
+        System.Text.RegularExpressions.Regex regex = new System.Text.RegularExpressions.Regex(pattern);
+        string result = regex.Replace(input, replacement);
+        return result;
+    }
+
     private void StartVideoRecording()
     {
         // Start recording the video
         // Add your video recording logic here
+        var videoAttr = new VideoTrackAttributes
+        {
+            frameRate = new MediaRational(60),
+            width = 640,
+            height = 480,
+            includeAlpha = false
+        };
+
+        videoFileName = GenerateFilename("ksteam");
+
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            videoOutputPath = Path.Combine(Application.persistentDataPath, videoFileName);
+        }
+        else if (Application.platform == RuntimePlatform.IPhonePlayer)
+        {
+            videoOutputPath = Path.Combine(Application.temporaryCachePath, videoFileName);
+        }
+        else if (Application.platform == RuntimePlatform.WindowsPlayer || Application.platform == RuntimePlatform.WindowsEditor)
+        {
+            videoOutputPath = Path.Combine(videoOutputRootPath, videoFileName);
+        }
+        else if (Application.isEditor)
+        {
+            videoOutputPath = Path.Combine(videoOutputRootPath, videoFileName);
+        } else
+        {
+            videoOutputPath = Path.Combine(videoOutputRootPath, videoFileName);
+        }
+
+        Debug.Log(videoOutputPath);
+
+        mediaEncoder = new MediaEncoder(videoOutputPath, videoAttr);
         isRecording = true;
     }
 
@@ -258,6 +385,14 @@ public class SceneManager : MonoBehaviour
         // Start recording the video
         // Add your video recording logic here
         isRecording = false;
+        mediaEncoder.Dispose();
+
+        if (Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer)
+        {
+            NativeGallery.SaveVideoToGallery(videoOutputPath, "K-Steam", videoFileName);
+        }
+
+        Debug.Log($"Video is savedat {videoOutputPath}.");
     }
 
 
@@ -273,7 +408,7 @@ public class SceneManager : MonoBehaviour
         {
             player.Play(youtubeURL[songIndex], timePlaying);
         }
-        
+
     }
 
     private void Reset()
